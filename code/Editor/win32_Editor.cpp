8e62177170aca1d4bdde1cd844cc3406bba0f00c
Øystein <oystein.brandt@gmail.com>
2017-11-06 00:29:17 +0100

    Initial commit of windows specific entry point

---
+++ code/Editor/win32_Editor.cpp
@@ -0,0 +1,112 @@
+#include <Windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK mainWindowCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT result{ 0 };
+	switch (message)
+	{
+	case WM_SIZE:
+	{
+		OutputDebugStringA("WM_SIZE\n");
+	}break;
+
+	case WM_DESTROY:
+	{
+		OutputDebugStringA("WM_DESTROY\n");
+	}break;
+
+	case WM_CLOSE:
+	{
+		OutputDebugStringA("WM_CLOSE\n");
+	} break;
+
+	case WM_ACTIVATEAPP:
+	{
+		OutputDebugStringA("WM_ACTIVATEAPP\n");
+	}break;
+
+	default:
+		result = DefWindowProc(window, message, wParam, lParam);
+		break;
+	}
+
+	return result;
+}
+
+int CALLBACK 
+WinMain(HINSTANCE hInstance,
+				HINSTANCE hPrevInstance,
+				LPSTR lpCmdLine,
+				int nCmdShow)
+{
+	WNDCLASSEX windowClass = {};
+
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	windowClass.lpfnWndProc = mainWindowCallback;
+	windowClass.hInstance = hInstance;
+	//windowClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClass.lpszMenuName = NULL;
+	windowClass.lpszClassName = "GraphicsPlaygroundEditorWindowClass";
+	//windowClass.hIconSm = LoadIcon(windowClass.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&windowClass))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	static TCHAR szWindowClass[] = _T("win32app");
+	static TCHAR szTitle[] = _T("Win32 Guided Tour Application");
+
+	// The parameters to CreateWindow explained:  
+	// szWindowClass: the name of the application  
+	// szTitle: the text that appears in the title bar  
+	// WS_OVERLAPPEDWINDOW: the type of window to create  
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)  
+	// 500, 100: initial size (width, length)  
+	// NULL: the parent of this window  
+	// NULL: this application does not have a menu bar  
+	// hInstance: the first parameter from WinMain  
+	// NULL: not used in this application  
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
